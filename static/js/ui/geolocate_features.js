eed7f9e2bf1475700aba82d645f30b8698cf5da8
meyerjo <meyerjo@tf.uni-freiburg.de>
2022-10-13 12:09:49 +0200

    geolocaate_features

--- static/js/ui/geolocate_features.js
+++ static/js/ui/geolocate_features.js
@@ -1,28 +1,27 @@
-    var moving_marker = null;
-    var markerCurrentPosition = null;
-    var locationMarker = null;
-    const options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    };
 
-    function success(pos) {
-        const crd = pos.coords;
-        window.currentPosition = crd
-        console.log(`Your current position is: Latitude : ${crd.latitude}; Longitude: ${crd.longitude}; More or less ${crd.accuracy} meters.`);
+var locationMarker = null;
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
 
-        // Show a market at the position of the Eiffel Tower
-        locationMarker = L.marker([crd.latitude, crd.longitude]);
-        locationMarker.addTo(window.map);
-        updatePositionSpecificCues();
-    }
+function success(pos) {
+    const crd = pos.coords;
+    window.currentPosition = crd
+    console.log(`Your current position is: Latitude : ${crd.latitude}; Longitude: ${crd.longitude}; More or less ${crd.accuracy} meters.`);
 
-    function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
+    // Show a market at the position of the Eiffel Tower
+    locationMarker = L.marker([crd.latitude, crd.longitude]);
+    locationMarker.addTo(window.map);
+    updatePositionSpecificCues();
+}
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
 
 
-    function get_currentPosition() {
-        navigator.geolocation.getCurrentPosition(success, error, options);
-    }
+function get_currentPosition() {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+}
